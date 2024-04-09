98a5ca28036370eca473a4fbce1a42f37e9a54c0
spragul <spragul33@gmail.com>
2024-04-09 12:16:22 +0530

    commit

--- src/components/header/Action.js
+++ src/components/header/Action.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Action = () => {
   return (
     <div className='action'>
-        <a className='btn' href='https://drive.google.com/file/d/10Txu47bWWoxzqEjNR4wd3jZZiUSUWfwO/view?usp=sharing' target='_blank' download>Download Resume</a>
+        <a className='btn' href='https://drive.google.com/file/d/1PFRY20qCUbEB6zyPFQbCFzwli_6aadBL/view?usp=sharing' target='_blank' download>Download Resume</a>
         <a className='btn btn-primary' href="#contact">Contact me!</a>
     </div>
   )
